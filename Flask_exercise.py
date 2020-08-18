ad91392ef67728bb5e0cac1175b8e18c75884ea5
seraht <serah1995@hotmail.fr>
2020-08-18 21:23:25 +0300

    predictor html

--- Flask_exercise.py
+++ Flask_exercise.py
@@ -18,29 +18,9 @@ clf2 = pd.read_pickle("model.pkl")
 app = Flask(__name__)
 
 
-@app.route('/',methods=['GET', 'POST'])
-def main():
-    if request.method == 'GET':
-        return render_template('main.html')
-
-    if request.method == 'POST':
-        is_male =  request.form['is_male']
-        num_interactions_with_cust_service =  request.form['num_interactions_with_cust_service']
-        late_on_payment =  request.form['late_on_payment']
-        age =  request.form['age']
-        years_in_contract =  request.form['years_in_contract']
-        input_variables = pd.DataFrame([['is_male', 'num_interactions_with_cust_service', 'late_on_payment', 'age', 'years_in_contract']],
-columns=['is_male', 'num_interactions_with_cust_service', 'late_on_payment', 'age', 'years_in_contract'], dtype=float)
-
-
-        # Predicting the Wine Quality using the loaded model
-        prediction = clf2.predict(input_variables)[0]
-        if prediction:
-            pred = "The customer is likely to churn"
-        else:
-            pred = "He is a loyal customer"
-        return render_template('main.html', result=pred)
-
+@app.route('/')
+def main_page():
+    return 'API for CHURNED predictions'
 
 
 @app.route('/predict')
@@ -75,6 +55,25 @@ def predict_multiple():
             responses[f"Customer {i + 1}"] = f"Input: {', '.join([str(values.loc[i, :][j]) for j in range(values.shape[1])])}, Output: is a loyal customer"
 
     return make_response(jsonify(responses))
+
+@app.route('/p')
+def predict2():
+    to_predict = np.zeros(5).reshape(1, 5)
+    features = ['is_male', 'num_interactions_with_cust_service', 'late_on_payment', 'age', 'years_in_contract']
+    for i, feat in enumerate(features):
+        if request.args.get(feat) is not None:
+            to_predict[0][i] = request.args.get(feat)
+
+    response = clf2.predict(to_predict)
+
+    if response:
+        pred = "The customer is likely to churn"
+    else:
+        pred = "He is a loyal customer"
+    try:
+        return render_template('predict.html', something=pred)
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     port = os.environ.get('PORT')
